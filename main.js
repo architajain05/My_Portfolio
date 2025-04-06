4fbbbf75f875a9ef463943c90270606f48d43ff4
Archita <122664337+architajain05@users.noreply.github.com>
2025-04-06 17:54:01 +0530

    files uploaded

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".text",{
+    strings: [ "Software Developer", "Frontend Developer","Cloud Computing Enthusiast!!"],
+    typeSpeed: 60,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+})
